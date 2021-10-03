dde746c4f26220572c2daabc38e72175b1e93ae0
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-10-04 01:58:28 +0700

    model

---
+++ app/src/main/java/tictactoe/model/Difficult.java
@@ -0,0 +1,12 @@
+public enum Difficult {
+    EASY("Easy"),
+    MEDIUM("Medium"),
+    HARD("hard"),
+    IMPOSSIBLE("Impossible");
+
+    public final String title;
+
+    Difficult(String title) {
+        this.title = title;
+    }
+}
